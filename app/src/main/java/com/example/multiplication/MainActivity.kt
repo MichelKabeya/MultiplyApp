3f33926402b32e9be6ee80474a41d35fee4fcdf0
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-05-21 09:59:27 +0200

    added a background

--- app/src/main/java/com/example/multiplication/MainActivity.kt
+++ app/src/main/java/com/example/multiplication/MainActivity.kt
@@ -11,17 +11,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
+      // created a function that will perform calculation
     private fun multiply(){
+        //declared the variable that will hold the final result
         val numProduct: Int = value1 * value2
         answer.text = numProduct.toString()
     }
-    var num1: EditText = findViewById<EditText>(R.id.editText1)
-    var num2: EditText = findViewById<EditText>(R.id.editText2)
+    var num1 = findViewById<EditText>(R.id.editText1)
+    var num2 = findViewById<EditText>(R.id.editText2)
     var value1 = num1.text.toString().toInt()
     var value2 = num2.text.toString().toInt()
     var answer = findViewById<TextView>(R.id.textAns)
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()

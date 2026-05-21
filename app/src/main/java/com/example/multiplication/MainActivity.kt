cc2e651a332c16ee76a67c6d9a10970645ecd159
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-05-21 09:27:05 +0200

    created a function "Multiply"

--- app/src/main/java/com/example/multiplication/MainActivity.kt
+++ app/src/main/java/com/example/multiplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.multiplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -8,21 +9,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-    var num1 = findViewById<EditText>(R.id.editText1)
-    var num2 = findViewById<EditText>(R.id.editText2)
-    val calBtn = findViewById<Button>(R.id.multiplyBtn)
-    fun multiply(){
-        var numProduct = 0
-        numProduct = num1 * num2
+
+    private fun multiply(){
+        val numProduct: Int = value1 * value2
+        answer.text = numProduct.toString()
     }
+    var num1: EditText = findViewById<EditText>(R.id.editText1)
+    var num2: EditText = findViewById<EditText>(R.id.editText2)
+    var value1 = num1.text.toString().toInt()
+    var value2 = num2.text.toString().toInt()
+    var answer = findViewById<TextView>(R.id.textAns)
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        var answer = findViewById<TextView>(R.id.viewAnswer)
+
+        val calBtn = findViewById<Button>(R.id.multiplyBtn)
+
+        calBtn.setOnClickListener {
+            multiply()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

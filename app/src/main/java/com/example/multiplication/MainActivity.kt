934cbc736863de20816856f22f2ac09026216f24
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-05-21 09:05:47 +0200

    added a textview for the final answer

---
+++ app/src/main/java/com/example/multiplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.multiplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    var num1 = findViewById<EditText>(R.id.editText1)
+    var num2 = findViewById<EditText>(R.id.editText2)
+    val calBtn = findViewById<Button>(R.id.multiplyBtn)
+    fun multiply(){
+        var numProduct = 0
+        numProduct = num1 * num2
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var answer = findViewById<TextView>(R.id.viewAnswer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
